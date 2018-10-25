957877dbd693646e50427ea73e48b33356da4d34
Cesar Jolibois <diazcd85@gmail.com>
2018-10-25 12:15:48 -0600

    half way

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,12 @@
 class CartsController < ApplicationController
    def create
     song = Song.find(params[:song_id])
-    flash[:notice] = "Added #{song.title} to the cart"
+    id_string =song.id.to_s
+    session[:cart] ||= {}
+    session[:cart][id_string] ||= 0
+    session[:cart][id_string] += 1
+    quantity = session[:cart][id_string] + 1
+    flash[:notice] = "You Added #{pluralize(quantity, "Copy")} of #{song.title} to the cart"
     redirect_to songs_path
   end
 end
--- spec/features/user_can_add_songs_to_cart_spec.rb
+++ spec/features/user_can_add_songs_to_cart_spec.rb
@@ -15,9 +15,25 @@ describe 'Cart' do
     it { is_expected.to have_css("#to_the_cart-#{@song_1.id}") }
   end
 
-  context 'add items to the cart' do
+  context 'a message is send of the song added to cart' do
     subject{click_on "to_the_cart-#{@song_1.id}"; page}
 
     it { is_expected.to have_content("Added #{@song_1.title} to the cart") }
+  end
+
+  context 'the message increments for multiple songs' do
+    subject{click_on "to_the_cart-#{@song_1.id}"; page}
+
+    it { is_expected.to have_content("You added 1 copy of #{@song_1.title} to the cart") }
+    context 'add a song twice' do
+
+      subject do
+        click_on "to_the_cart-#{@song_1.id}"
+        click_on "to_the_cart-#{@song_1.id}"
+        page
+      end
+
+      it { is_expected.to have_content("You Added 2 Copies of #{@song_1.title} to the cart") }
+    end
   end
 end
